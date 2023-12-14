78c7858ab22f13b8ca65176ed609ff0cab46024f
jbucsa <justin.bucsa@gmail.com>
2023-12-13 21:20:15 -0600

    Completed Search using useRef but this creates an error.

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,52 +1,15 @@
-import { useState } from 'react' 
+import { useContext } from 'react'
+import { SearchContext } from '../context/SearchContext'
 
-function GalleryItem(props){
-    let [view, setView] = useState(false)
-
-    const simpleStyle = {
-        'width': '25vw',
-        'height': '30vh',
-        'border': '1px solid black',
-        'margin': '2px'
-    }
-    
-    const detailStyle = {
-        'width': '60vw',
-        'height': '30vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${props.item.artworkUrl100})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'yellow'
-    }
-
-    const simpleView = () => {
-        return (
-            <div style={simpleStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
-            </div>
-        )
-    }
-
-    const detailView = () => {
-        return (
-            <div style={detailStyle}>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
-            </div>
-        )
-    }
+function Searchbar() {
+    const {term, handleSearch} = useContext(SearchContext)
 
     return (
-        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
-                {view ? detailView() : simpleView()}
-        </div>
+        <form >
+            <input ref={term} type="text" placeholder="Enter a search term here"/>
+            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+        </form>
     )
 }
 
-export default GalleryItem
+export default Searchbar
